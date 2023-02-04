0c55c87f470ade471a1d2fd2fc5015c53f65d38b
duc.pd <ducpham.uet@gmail.com>
2023-02-04 16:27:53 +0700

    create roll button random score of dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scoreText: TextView = findViewById(R.id.score_text)
+        val rollBtn: Button = findViewById(R.id.roll_btn)
+        rollBtn.setOnClickListener {
+            val range: IntRange = 1..6
+            var score: Int = range.random();
+            scoreText.text = score.toString()
+        }
     }
 }
